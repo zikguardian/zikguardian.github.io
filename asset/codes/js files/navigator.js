1a3cd7bbe9db33d919f7ff76aea47edd1c8aabab
zikguardian <66210474+zikguardian@users.noreply.github.com>
2022-05-22 02:40:27 +0900

    2.0.0

--- asset/codes/js files/navigator.js
+++ asset/codes/js files/navigator.js
@@ -2,16 +2,16 @@ document.write(`
     <nav class="navbar">
         <div class="navbar_logo">
             <img src="https://zikguardian.github.io/asset/image/logo2.png" width="45" height="45" alt="Logo">
-            <a href="https://zikguardian.github.io/index.html">DNA Games</a>
+            <a href="https://zikguardian.github.io">DNA Games</a>
         </div>
 
         <ul class="navbar_menu">
-            <li><a href="https://zikguardian.github.io/index.html" class="nav_btn">Home</a></li>
-            <li><a href="https://zikguardian.github.io/about/about.html " class="nav_btn">DNA 소개</a></li>
-            <li><a href="https://zikguardian.github.io/games/games.html" class="nav_btn">게임들</a></li>
-            <li><a href="https://zikguardian.github.io/fqa/fqa.html" class="nav_btn">자주 묻는 질문</a></li>
-            <li><a href="https://zikguardian.github.io/with_us/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
-            <li><a href="https://zikguardian.github.io/social_impact/social_impact.html" class="nav_btn">사회공헌</a></li>
+            <li><a href="https://zikguardian.github.io" class="nav_btn">Home</a></li>
+            <li><a href="https://zikguardian.github.io/Website/html/about.html " class="nav_btn">DNA 소개</a></li>
+            <li><a href="https://zikguardian.github.io/Website/html/games.html" class="nav_btn">게임들</a></li>
+            <li><a href="https://zikguardian.github.io/Website/html/fqa.html" class="nav_btn">자주 묻는 질문</a></li>
+            <li><a href="https://zikguardian.github.io/Website/html/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
+            <li><a href="https://zikguardian.github.io/Website/html/social_impact.html" class="nav_btn">사회공헌</a></li>
         </ul>
 
         <ul class="navbar_icons">
