b3b8d3317a004c702fd866f8f3bd75342e3fe905
zikguardian <66210474+zikguardian@users.noreply.github.com>
2022-05-22 02:53:04 +0900

    2.0.0

--- asset/codes/js files/navigator.js
+++ asset/codes/js files/navigator.js
@@ -7,18 +7,18 @@ document.write(`
 
         <ul class="navbar_menu">
             <li><a href="https://zikguardian.github.io" class="nav_btn">Home</a></li>
-            <li><a href="https://zikguardian.github.io/Website/html/about.html " class="nav_btn">DNA 소개</a></li>
-            <li><a href="https://zikguardian.github.io/Website/html/games.html" class="nav_btn">게임들</a></li>
-            <li><a href="https://zikguardian.github.io/Website/html/fqa.html" class="nav_btn">자주 묻는 질문</a></li>
-            <li><a href="https://zikguardian.github.io/Website/html/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
-            <li><a href="https://zikguardian.github.io/Website/html/social_impact.html" class="nav_btn">사회공헌</a></li>
+            <li><a href="https://zikguardian.github.io/Website/about.html " class="nav_btn">DNA 소개</a></li>
+            <li><a href="https://zikguardian.github.io/Website/games.html" class="nav_btn">게임들</a></li>
+            <li><a href="https://zikguardian.github.io/Website/faq.html" class="nav_btn">자주 묻는 질문</a></li>
+            <li><a href="https://zikguardian.github.io/Website/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
+            <li><a href="https://zikguardian.github.io/Website/social_impact.html" class="nav_btn">사회공헌</a></li>
         </ul>
 
         <ul class="navbar_icons">
             <a href="https://zikguardian.github.io/asset/codes/login/login.html">로그인</a>
             <li></li>
-            <li><a href="http://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="http://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
         </ul>
 
         <a href="#" class="navbar_toogleBtn">
