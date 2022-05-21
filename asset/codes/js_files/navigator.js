416caa438462d9f9f689614b7731ef6e410ba78f
zikguardian <66210474+zikguardian@users.noreply.github.com>
2022-05-22 03:04:52 +0900

    Add files via upload

--- asset/codes/js_files/navigator.js
+++ asset/codes/js_files/navigator.js
@@ -1,21 +1,21 @@
 document.write(`
     <nav class="navbar">
         <div class="navbar_logo">
-            <img src="zikguardian.github.io/asset/image/logo2.png" width="45" height="45" alt="Logo">
-            <a href="zikguardian.github.io">DNA Games</a>
+            <img src="https://zikguardian.github.io/asset/image/logo2.png" width="45" height="45" alt="Logo">
+            <a href="https://zikguardian.github.io">DNA Games</a>
         </div>
 
         <ul class="navbar_menu">
-            <li><a href="zikguardian.github.io" class="nav_btn">Home</a></li>
-            <li><a href="zikguardian.github.io/Website/about.html " class="nav_btn">DNA 소개</a></li>
-            <li><a href="zikguardian.github.io/Website/games.html" class="nav_btn">게임들</a></li>
-            <li><a href="zikguardian.github.io/Website/faq.html" class="nav_btn">자주 묻는 질문</a></li>
-            <li><a href="zikguardian.github.io/Website/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
-            <li><a href="zikguardian.github.io/Website/social_impact.html" class="nav_btn">사회공헌</a></li>
+            <li><a href="https://zikguardian.github.io" class="nav_btn">Home</a></li>
+            <li><a href="https://zikguardian.github.io/Website/about.html " class="nav_btn">DNA 소개</a></li>
+            <li><a href="https://zikguardian.github.io/Website/games.html" class="nav_btn">게임들</a></li>
+            <li><a href="https://zikguardian.github.io/Website/faq.html" class="nav_btn">자주 묻는 질문</a></li>
+            <li><a href="https://zikguardian.github.io/Website/with_us.html" class="nav_btn">DNA와 함께하기</a></li>
+            <li><a href="https://zikguardian.github.io/Website/social_impact.html" class="nav_btn">사회공헌</a></li>
         </ul>
 
         <ul class="navbar_icons">
-            <a href="zikguardian.github.io/asset/codes/login/login.html">로그인</a>
+            <a href="https://zikguardian.github.io/asset/codes/login/login.html">로그인</a>
             <li></li>
             <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
