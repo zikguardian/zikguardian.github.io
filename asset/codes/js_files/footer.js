287bd1699a85378161bf1fa5d5d0e258a68ad666
zikguardian <66210474+zikguardian@users.noreply.github.com>
2022-06-07 14:15:45 +0900

    2.0.1.1
    시스템 안정화

---
+++ asset/codes/js_files/footer.js
@@ -0,0 +1,17 @@
+document.write(`
+    <style>
+            ul li {
+                list-style-type: none;
+                display: inline;
+                margin-left: 20px;
+            }
+    </style>
+    <hr>
+    <h3>DNA Games 팔로우 하기</h3>
+    <ul>
+        <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
+        <li><a href="https://youtube.com" target="_blank"><i class="fab fa-youtube"></i></a></li>
+    </ul>
+    <p>Copyright © 2022 DNA Games. All rights reserved.</p>
+`);
