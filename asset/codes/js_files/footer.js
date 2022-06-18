c1b37d7ad271c9036a38637fa7ad6b511711b330
zikguardian <66210474+zikguardian@users.noreply.github.com>
2022-06-19 04:17:18 +0900

    2.1.0
    부족한 부분을 보완하고 조금 더 채워 넣었습니다.

--- asset/codes/js_files/footer.js
+++ asset/codes/js_files/footer.js
@@ -6,13 +6,32 @@ document.write(`
                 margin-left: 20px;
             }
     </style>
+
     <br><br>
+
     <hr>
+
     <h3>DNA Games 팔로우 하기</h3>
     <ul>
+        <li><a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
         <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
         <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
         <li><a href="https://youtube.com" target="_blank"><i class="fab fa-youtube"></i></a></li>
     </ul>
-    <p>Copyright © 2022 DNA Games. All rights reserved.</p>
+
+    <hr>
+
+    <div class="help">
+    <img src="http://zikguardian.github.io/asset/image/logo2.png" alt="" class="DNA_Logo">
+        <a href="http://zikguardian.github.io/asset/codes/html/policy.html target="_blank">개인정보처리방침</a>
+        <a href="http://zikguardian.github.io/asset/codes/html/legal.html target="_blank">서비스 약관</a>
+        <a href="http://zikguardian.github.io/asset/codes/html/support.html target="_blank">고객지원</a>
+    </div>
+
+    <div class="DNA_info">
+        <p>디엔에이 게임즈 (DNA Games)</p>
+        <p>부산광역시 북구 금곡대로 285번길 40, 503호 (화명동, 한빛프라자)</p>
+        <p>사업자등록번호 : 154-49-00873</p>
+        <p>© DNA Games All Rights Reserved.</p>
+    </div>
 `);
