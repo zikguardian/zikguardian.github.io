b024eda5a8736d0e3ba99dce768679806e556631
zikguardian <66210474+zikguardian@users.noreply.github.com>
2024-05-20 00:16:06 +0900

    Add files via upload

---
+++ js/Footer.js
@@ -0,0 +1,40 @@
+document.getElementById("Footer").innerHTML = `
+<header class="style1">
+</header>
+<section id="footer" class="wrapper">
+  <div class="title">Information</div>
+  <div class="container">
+    <h3 style="text-align: center;">한국어&emsp;▲</h3> <br>
+    <img src="./images/DNAGamesIncLogo.png"
+      style="height: 100px; width:100px; margin-left: auto; margin-right: auto; display: block" alt="">
+  </div>
+  <br>
+  <div style="text-align: center;">
+    <a href="https://twitter.com">X</a>&emsp;
+    <a href="https://www.facebook.com">FB</a>&emsp;
+    <a href="https://www.reddit.com">R</a>&emsp;
+    <a href="https://www.instagram.com">I</a>&emsp;
+    <a href="https://www.youtube.com/@DNAGamesInc">YT</a>
+  </div>
+  <br>
+  <div style="text-align:center;">
+    <a style="font-size:18px; color:grey;" href="#">개인정보 처리방침</a>&emsp;
+    <a style="font-size:18px; color:grey;" href="#">법률 관련 문서</a>&emsp;
+    <a style="font-size:18px; color:grey;" href="#">이용 약관</a>&emsp;
+    <a style="font-size:18px; color:grey;" href="#">쿠키정보 처리방침</a>&emsp;
+    <a style="font-size:18px; color:grey;" href="#">쿠키 설정</a>
+  </div>
+  <br>
+  <div style="text-align:center;">
+    <p style="font-size:15px; color:grey;">
+      Copyright 2024 DNA Games Inc. All Right Reserved.<br>
+      주식회사 디엔에이게임즈(DNA Games Inc.)<br>
+      주소:부산광역시 해운대구 세실로 79, 8층 69호 (48106)<br>
+      이메일:dnagames2022@gmail.com<br>
+      사업자 등록 번호:895-88-02821 | 통신판매업신고번호:2024-부산해운대-0741 | <a style="font-size:15px; color:grey;"
+        href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=8958802821">사업자정보확인</a><br>
+      대표이사:장동혁
+    </p>
+  </div>
+</section>
+`;
---
+++ js/TopNav.js
@@ -0,0 +1,64 @@
+document.getElementById("nav").innerHTML = `
+  
+<nav id="nav">
+				<ul>
+					<li class="current"><a href="index.html">Home</a>
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">소개</a></li>
+							<li><a href="#">역사</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="Games.html">게임</a>
+						<ul>
+							<li><a href="./Games/SpaceHeroes.html">스페이스 히어로즈 (Space Heroes)</a></li>
+							<li><a href="#">2번 게임</a></li>
+							<li><a href="#">3번 게임</a></li>
+							<li>
+								<a href="#">4번 게임</a>
+
+							</li>
+							<li><a href="#">에그 시리즈</a>
+								<ul>
+									<li><a href="./Games/EggSeries/JumpingEgg.html">점핑 에그</a></li>
+									<li><a href="./Games/EggSeries/ArrowEgg.html">애로우 에그</a></li>
+									<li><a href="./Games/EggSeries/HuntingEgg.html">헌팅 에그</a></li>
+								</ul>
+							</li>
+							<li><a href="#">트레이서 시리즈</a>
+								<ul>
+									<li><a href="./Games/TracerSeries/TargetTracer.html">타겟 트레이서</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+					<li><a href="Brand.html">문화</a>
+						<ul>
+							<li><a href="#">기업 문화</a></li>
+							<li><a href="#">복지</a></li>
+						</ul>
+					</li>
+					<li><a href="Careers.html">채용</a>
+						<ul>
+							<li><a href="#">채용소개</a></li>
+							<li><a href="#">지원하기</a></li>
+							<li><a href="#">모르겠다면</a></li>
+						</ul>
+					</li>
+					<li><a href="IR.html">투자정보</a>
+						<ul>
+							<li><a href="#">기업지배구조</a></li>
+							<li><a href="#">주가정보</a></li>
+							<li><a href="#">재무정보</a></li>
+							<li>
+								<a href="#">공시 및 공고</a>
+							</li>
+							<li><a href="#">IR자료</a></li>
+							<li><a href="#">지속가능경영</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+
+		</section>`;
