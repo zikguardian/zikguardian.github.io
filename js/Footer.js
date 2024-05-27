e9c30bbe2158533eb2e5b5cee26eb9ae1d17c81f
zikguardian <66210474+zikguardian@users.noreply.github.com>
2024-05-28 01:08:11 +0900

    Add files via upload

--- js/Footer.js
+++ js/Footer.js
@@ -10,19 +10,19 @@ document.getElementById("Footer").innerHTML = `
   </div>
   <br>
   <div style="text-align: center;">
-    <a href="https://twitter.com">X</a>&emsp;
-    <a href="https://www.facebook.com">FB</a>&emsp;
-    <a href="https://www.reddit.com">R</a>&emsp;
-    <a href="https://www.instagram.com">I</a>&emsp;
-    <a href="https://www.youtube.com/@DNAGamesInc">YT</a>
+    <a href="https://twitter.com" style="text-decoration:none">X</a>&emsp;
+    <a href="https://www.facebook.com" style="text-decoration:none">FB</a>&emsp;
+    <a href="https://www.reddit.com" style="text-decoration:none">R</a>&emsp;
+    <a href="https://www.instagram.com" style="text-decoration:none">I</a>&emsp;
+    <a href="https://www.youtube.com/@DNAGamesInc" style="text-decoration:none">YT</a>
   </div>
   <br>
   <div style="text-align:center;">
-    <a style="font-size:18px; color:grey;" href="#">개인정보 처리방침</a>&emsp;
-    <a style="font-size:18px; color:grey;" href="#">법률 관련 문서</a>&emsp;
-    <a style="font-size:18px; color:grey;" href="#">이용 약관</a>&emsp;
-    <a style="font-size:18px; color:grey;" href="#">쿠키정보 처리방침</a>&emsp;
-    <a style="font-size:18px; color:grey;" href="#">쿠키 설정</a>
+    <a style="text-decoration:none; font-size:18px; color:grey;" href="#">개인정보 처리방침</a>&emsp;
+    <a style="text-decoration:none; font-size:18px; color:grey;" href="#">법률 관련 문서</a>&emsp;
+    <a style="text-decoration:none; font-size:18px; color:grey;" href="#">이용 약관</a>&emsp;
+    <a style="text-decoration:none; font-size:18px; color:grey;" href="#">쿠키정보 처리방침</a>&emsp;
+    <a style="text-decoration:none; font-size:18px; color:grey;" href="#">쿠키 설정</a>
   </div>
   <br>
   <div style="text-align:center;">
@@ -31,7 +31,7 @@ document.getElementById("Footer").innerHTML = `
       주식회사 디엔에이게임즈(DNA Games Inc.)<br>
       주소:부산광역시 해운대구 세실로 79, 8층 69호 (48106)<br>
       이메일:dnagames2022@gmail.com<br>
-      사업자 등록 번호:895-88-02821 | 통신판매업신고번호:2024-부산해운대-0741 | <a style="font-size:15px; color:grey;"
+      사업자 등록 번호:895-88-02821 | 통신판매업신고번호:2024-부산해운대-0741 | <a style="text-decoration:none; font-size:15px; color:grey;"
         href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=8958802821">사업자정보확인</a><br>
       대표이사:장동혁
     </p>
