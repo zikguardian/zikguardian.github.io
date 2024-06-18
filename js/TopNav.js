ed7d5c995a94e5b59e0835b2b160f9e6be5f519b
zikguardian <66210474+zikguardian@users.noreply.github.com>
2024-06-18 16:58:41 +0900

    Add files via upload

--- js/TopNav.js
+++ js/TopNav.js
@@ -5,7 +5,7 @@ document.getElementById("nav").innerHTML = `
 					<li class="current"><a href="index.html">홈</a>
 						<ul>
 							<li><a href="http://www.dnagamescorp.com">Home</a></li>
-							<li><a href="./Intro.html">소개</a>
+							<li><a href="./About.html">소개</a>
 								<ul>
 										<li><a href="#">Dream</a></li>
 										<li><a href="#">Adventure</a></li>
@@ -56,16 +56,16 @@ document.getElementById("nav").innerHTML = `
 							<li><a href="#">모르겠다면</a></li>
 						</ul>
 					</li>
-					<li><a href="IR.html">투자정보</a>
+					<li><a href="ESG.html">IR</a>
 						<ul>
-							<li><a href="#">기업지배구조</a></li>
-							<li><a href="#">주가정보</a></li>
-							<li><a href="#">재무정보</a></li>
+							<li><a href="./Structure.html">기업지배구조</a></li>
+							<li><a href="Stockinfo.html">주가정보</a></li>
+							<li><a href="Financial.html">재무정보</a></li>
 							<li>
 								<a href="#">공시 및 공고</a>
 							</li>
-							<li><a href="#">IR자료</a></li>
-							<li><a href="#">지속가능경영</a></li>
+							<!--<li><a href="#">IR자료</a></li>
+							<li><a href="#">지속가능경영</a></li>-->
 						</ul>
 					</li>
 				</ul>
